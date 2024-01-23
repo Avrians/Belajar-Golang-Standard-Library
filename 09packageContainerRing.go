00b6c35e9c2e29042b46e313a7c1ae5f0c2201d5
avrians <avriansyahb10@gmail.com>
2024-01-23 11:38:20 +0700

    [av][package sort]

--- 09packageContainerRing.go
+++ 09packageContainerRing.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 )
 
-func main()  {
+func main09()  {
 	var data *ring.Ring = ring.New(5)
 
 	// Manual
---
+++ 10packSort.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+type User struct {
+	Name string
+	Age int
+}
+
+type UserSlice []User
+
+func (s UserSlice) Len() int {
+	return len(s)
+}
+
+func (s UserSlice) Less(i, j int) bool {
+	return s[i].Age < s[j].Age
+}
+
+func (s UserSlice) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+
+func main()  {
+	users := []User{
+		{"John", 20},
+		{"Jane", 30},
+		{"Doe", 25},
+		{"Foo", 10},
+	}
+
+	sort.Sort(UserSlice(users))
+
+	fmt.Println(users)
+}
